5aeafa37b120884c2a73738d36169f8ad0692312
Сергей Симонов <s.simonov@bars-open.ru>
2022-06-13 23:04:08 +0300

    Block 1: Memory usage

--- block_1/memory_usage/task_1/implementation.py
+++ block_1/memory_usage/task_1/implementation.py
@@ -1,5 +1,15 @@
+import os
+import weakref
+
+
 class TempFile:
 
     def __init__(self, path) -> None:
         super().__init__()
         self._f = open(path, 'wb')
+        weakref.finalize(self, self._delete_file, self._f)
+
+    @staticmethod
+    def _delete_file(file):
+        file.close()
+        os.remove(file.name)
